3e75171a8090ef20b0bdb1e0fd40bb2fd2682297
Emmanuwil <simonemmanuwil@gmail.com>
2022-02-03 09:28:55 -0500

    Learning Python

---
+++ ex9.py
@@ -0,0 +1,14 @@
+# Here's somes new strange stuff, remember type it exactly
+
+days = "Mon Tues Wed Thu Fry Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days; ", days)
+print("Here are the months: ", months)
+
+print("""
+There's somthing going on here.
+With three double-quotes.
+We'll being to able to type as much as we like.
+Well 4 lines if we want, or 5, or 6.
+""")
---
+++ inputs.py
@@ -0,0 +1,14 @@
+print("Enter You Name")
+z = input()
+print("Hi there," + z)
+
+username = input("Enter username: ")
+print("Username is: " + username)
+
+print("what is your birthday?")
+w = input()
+print("Oh so you were born on " + w)
+
+print("What is you cell phone nuumber? ")
+y = input()
+print("Thanks I will call you later.")
