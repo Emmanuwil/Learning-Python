b8728f8dd5bcef6f90ec6f4ba2e719efd3d7e6a7
Emmanuwil <simonemmanuwil@gmail.com>
2022-02-04 23:12:55 -0500

    Learning Python

---
+++ variables.py
@@ -0,0 +1,29 @@
+myvar = "Emmanuwil"
+my_var = "Emmanuwil"
+_my_var = "Emmanuwil"
+myVar = "Emmanuwil"
+MYVAR = "Emmanuwil"
+myvar2 = "Emmanuwil"
+
+myVariableName = "EmmanuwilSimon"
+MyVariableName = "EmmanuwilSimon"
+my_variable_name = "EmmanuwilSimon"
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+print(myVariableName)  #Camel case
+print(MyVariableName)  #Pascal case
+print(my_variable_name) #Snake case
+
+
+# Python Varible - Assign Multiple Varibles
+
+x, y, z = "Python", "Javascript", "Rust"
+
+print(x)
+print(y)
+print(z)
