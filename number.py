58270006fc1cad3c1b2674db3737acda1f753ba2
Emmanuwil <simonemmanuwil@gmail.com>
2022-02-09 11:41:10 -0500

    learning python

---
+++ number.py
@@ -0,0 +1,73 @@
+# IN Python its 3 types of number. int, float, complex.
+
+"""
+x = int
+y = 2.8
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#example
+
+r = 93939393
+p = 746464
+e = 635354
+
+print(type(r))
+print(type(p))
+print(type(e))
+
+xx= 1.1
+yy = 1.0
+zz = -35.59
+
+print(type(xx))
+print(type(yy))
+print(type(zz))
+
+xxx = 35e3
+yyy = 12E4
+zzz = -87.7e100
+
+
+print(type(xxx))
+print(type(yyy))
+print(type(zzz))
+
+
+
+a = 3+5j
+b = 5j
+c = -5j
+
+print(type(a))
+print(type(b))
+print(type(c))
+"""
+
+
+#Type Cnversion
+
+x = 1 # int
+y = 2.8 # float
+z = 1j #complex
+
+#convert from int to float
+a = float(x)
+
+#convert from float to int
+
+b = int(y)
+
+#convert from int to complex
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
