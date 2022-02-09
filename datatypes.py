0e34df02c2bedd8c2f598e2df615e8c0f6a81433
Emmanuwil <simonemmanuwil@gmail.com>
2022-02-09 02:40:48 -0500

    learning python

---
+++ datatypes.py
@@ -0,0 +1,155 @@
+#Setting for data type
+"""
+e = "Emmanuwil"
+
+print(e)
+print(type(e))
+
+b = 20
+
+print(b)
+print(type(b))
+
+c = 30.9
+
+print(c)
+print(type(c))
+
+
+d = 1j
+
+print(d)
+print(type(d))
+
+a = ["iphone", "android", "blackberry"]
+
+print(a)
+print(type(a))
+
+f = ("pizza", "apples", "morimga")
+
+print(f)
+print(type(f))
+
+x = range(610)
+
+print(x)
+print(type(x))
+
+u = {"name" : "Eman", "age" : "27"}
+
+print(u)
+print(type(u))
+
+n = {"nick", "nathen", "north"}
+
+print(n)
+print(type(n))
+
+t = True
+
+print(t)
+print(type(t))
+
+fa = False
+
+print(fa)
+print(type(fa))
+
+
+g = b"Hello"
+
+print(g)
+print(type(g))
+
+
+y = frozenset({"apple", "banana", "cherry"})
+
+print(y)
+print(type(y))
+
+s = memoryview(bytes(5))
+
+print(s)
+print(type(s))
+
+m = bytearray(5)
+
+print(m)
+print(type(m))
+"""
+
+#Setting the specific data type. / If you want to specficy the data type you can use the folloing constuctor funtion
+
+x = str("Hello World")
+
+print(x)
+print(type(x))
+
+xx = str("whats up sis")
+
+print(xx)
+print(type(x))
+
+xr = str("Iphone XR")
+
+print(xr)
+print(type(xr))
+
+
+tt = 99.9
+
+print(tt)
+print(type(tt))
+
+c = complex(1j)
+
+print(c)
+print(type(c))
+
+
+r = list(("race", "rat", "rental"))
+
+print(r)
+print(type(r))
+
+k = tuple(("kim", "kate", "kiss"))
+
+print(k)
+print(type(k))
+
+jj = range(10000)
+
+print(jj)
+print(type(jj))
+
+rr = dict(name ="John", age=38)
+
+print(rr)
+print(type(rr))
+
+li = set(("apple", "pie,", "cherry"))
+print(li)
+print(type(li))
+
+xxx = frozenset(("tv", "laptop", "tablet"))
+
+ee = bool(5)
+print(ee)
+print(type(ee))
+
+dw = bytes(5)
+
+print(dw)
+print(type(dw))
+
+
+qq = bytearray(5)
+
+print(qq)
+print(type(qq))
+
+gg = memoryview(bytes(5))
+
+print(gg)
+print(type(gg))
