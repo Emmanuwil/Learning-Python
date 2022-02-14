23b2ce48311c2cd3101c786f334248be4a4e09ff
Emmanuwil <simonemmanuwil@gmail.com>
2022-02-14 02:21:57 -0500

    learning python

--- strings.py
+++ strings.py
@@ -18,88 +18,68 @@ print("My moms birthday is next month in " + m + ". I love my mom.")
 print("My father birthday was last month in " + j + ". My father is gives me the best advice.")
 print("Aunt TT birthday is in " + a + ". She is my favorite aunt. What is the date?")
 x = input()
-
-a = "My name is Emmanuwil Simon and I'm a software Engineer that knows Javascript and Rust too."
-print(a[40])
-
-b = "I have 7 sibling in total snd I love all of them dearly."
-print(b[0])
-
-c = "I will work in tech for about 10 year and then start my own company."
-print(c[10])
-
-d = 'I love my mom so much'
-print(d[2])
-
-e = 'elphant in the room'
-print(e[8])
-
-f = 'Zaza is the best'
-print(f[8])
-
-g = 'strings are cool I guess'
-print(g[5])
-
-
-# to get srting length use len() fuction
-
-print(len(g))
-
-print(len(a))
-
-# check to see if a certain character is present in a string, we can use the keyword "in".
-
-print("free" in a)
-
-print("Emmanuwil" in a)
-
-print("dad" in b)
-
-print("Cheyenne" in b)
-
-print("10" in c)
-
-
-print("cool" in g)
-
-print("zaza" in f)
-
-print("Zaza" in f)
-
-print("happy" in g)
-
-print("room" in e)
-# looping through strings
-
-for f in "forex":
-    print(f)
-
-for s in "stock":
-    print(s)
-
-for c in "crypto":
-    print(c)
-
-for b in "bonds":
-    print(b)
-
-for i in "index":
-    print(i)
-
-for fu in "future":
-    print(fu)
-txt = "The Best things in life are free"
-if "free" in txt:
-    print("Yes, free is present")
-
-txt1 = "Kyrie Irving has the best handles in the NBA"
-if "NBA" in txt1:
-    print("You are absoluty correct")
-
-x = 100
-y = 200
-if x > y:
-    print("that correct")
-elif y > x:
-    print("NO your wrong. 200 is greater.")
 '''
+
+#slicing 
+
+aa = "Hello World"
+print(aa[2])
+
+bb = "The rams just won the supper bowl"
+print(bb[4])
+
+cc = ("The only tv series on power I watch is power")
+print(cc[0])
+
+dd = "Good Day"
+print(dd[5])
+
+ee = 'Eagles next year'
+print(ee[2])
+
+ff = 'friday is a work day'
+print(ff[7])
+
+gg = 'goodness sake'
+print(gg[3])
+
+# looping string
+
+for x in "football":
+    print (x)
+
+for mm in "Money and power":
+    print(mm)
+
+for ggg in "Good God Alamighty":
+    print(ggg)
+
+for zzz in "Zebras for breakfast":
+    print(zzz)
+
+for ccc in "Clowns are real":
+    print(ccc)
+
+for ttt in "I must have a job in teh before the end of the year":
+    print(ttt)
+
+for number in "1234567890":
+    print(number)
+
+for tea in "green black jeera moringa":
+    print(tea)
+
+for laptop in "hp & asus":
+    print(laptop)
+
+for ppp in "No loans ever again":
+    print(ppp)
+
+#length to get the length of a function use the len() function
+
+print(len(aa))
+print(len(bb))
+print(len(cc))
+print(len(dd))
+print(len(ff))
+print(len(gg))
