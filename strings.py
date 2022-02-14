ee42f265b89f8d0f1abb3f69df2267e9a7c7e6ec
Emmanuwil <simonemmanuwil@gmail.com>
2022-02-14 01:41:50 -0500

    learning pythom

---
+++ strings.py
@@ -0,0 +1,105 @@
+'''
+j = 'January'
+f = 'Febuary'
+m = 'March'
+a = 'April'
+mm = "May"
+ju = "June"
+jl = "July"
+au = "August"
+s = "September"
+o = "October"
+n = "November"
+d = "December"
+
+print('Today is ' + f + ' 14th/2022 ' )
+print('My younger sister birthday also is in ' + f + ". She is turning 18 this year. Time flys.")
+print("My moms birthday is next month in " + m + ". I love my mom.")
+print("My father birthday was last month in " + j + ". My father is gives me the best advice.")
+print("Aunt TT birthday is in " + a + ". She is my favorite aunt. What is the date?")
+x = input()
+
+a = "My name is Emmanuwil Simon and I'm a software Engineer that knows Javascript and Rust too."
+print(a[40])
+
+b = "I have 7 sibling in total snd I love all of them dearly."
+print(b[0])
+
+c = "I will work in tech for about 10 year and then start my own company."
+print(c[10])
+
+d = 'I love my mom so much'
+print(d[2])
+
+e = 'elphant in the room'
+print(e[8])
+
+f = 'Zaza is the best'
+print(f[8])
+
+g = 'strings are cool I guess'
+print(g[5])
+
+
+# to get srting length use len() fuction
+
+print(len(g))
+
+print(len(a))
+
+# check to see if a certain character is present in a string, we can use the keyword "in".
+
+print("free" in a)
+
+print("Emmanuwil" in a)
+
+print("dad" in b)
+
+print("Cheyenne" in b)
+
+print("10" in c)
+
+
+print("cool" in g)
+
+print("zaza" in f)
+
+print("Zaza" in f)
+
+print("happy" in g)
+
+print("room" in e)
+# looping through strings
+
+for f in "forex":
+    print(f)
+
+for s in "stock":
+    print(s)
+
+for c in "crypto":
+    print(c)
+
+for b in "bonds":
+    print(b)
+
+for i in "index":
+    print(i)
+
+for fu in "future":
+    print(fu)
+txt = "The Best things in life are free"
+if "free" in txt:
+    print("Yes, free is present")
+
+txt1 = "Kyrie Irving has the best handles in the NBA"
+if "NBA" in txt1:
+    print("You are absoluty correct")
+
+x = 100
+y = 200
+if x > y:
+    print("that correct")
+elif y > x:
+    print("NO your wrong. 200 is greater.")
+'''
